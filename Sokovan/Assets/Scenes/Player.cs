627bed612dd22befda731fe611cd8227a0b697a1
leeyesang <59430890+leeyesang@users.noreply.github.com>
2021-02-14 15:22:28 +0900

    first sokovan

---
+++ Sokovan/Assets/Scenes/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+
+    public float speed = 10f;
+    public Rigidbody playerRigidbody;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody.AddForce(0, 1000, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
