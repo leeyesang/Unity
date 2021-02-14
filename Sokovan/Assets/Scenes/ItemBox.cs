a8cbbab8495fa1d54401604f54f00ce3679c54f0
leeyesang <59430890+leeyesang@users.noreply.github.com>
2021-02-14 20:10:29 +0900

    Trigger Settings

---
+++ Sokovan/Assets/Scenes/ItemBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    private Renderer myRenderer;
+    public Color touchColor;
+    private Color originColor;
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 트리거인 콜라이더와 충돌할 때 자동으로 실행
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "EndPoint")
+        {
+            myRenderer.material.color = Color.blue;
+            Debug.Log("EndPOint 도달");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            myRenderer.material.color = originColor;
+            //Debug.Log("EndPOint 도달");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            myRenderer.material.color = touchColor;
+           // Debug.Log("EndPOint 도달");
+        }
+    }
+
+    // 일반 콜라이더와 충돌했을 때 자동으로 실행
+    /*void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("충돌했음");
+    }*/
+}
---
+++ Sokovan/Assets/Scenes/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    //초당 프레임 60....
+    void Update()
+    {
+        //연속 회전
+        //transform.Rotate(60, 60, 60);
+        //화면을 60번 깜빡이면 (초당 60프레임) 1/60
+        //화면을 200번 깜빡이면 (초당 200프레임) 1/200
+        // 60/200 * 200 =60
+
+        transform.Rotate(60 * Time.deltaTime, 60 * Time.deltaTime, 60 * Time.deltaTime);
+
+        //Time. deltaTime은 화면이 한번 깜빡이는 시간 = 1 프레임
+    }
+}
